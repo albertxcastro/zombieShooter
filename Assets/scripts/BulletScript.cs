92c1bcc2ed4a24df229a1ac42dd995c04ec2f13c
Matt <mapc.1993@hotmail.com>
2018-10-10 17:51:32 -0500

    A lot of ficses

---
+++ Assets/scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float bulletSpeed = 1;
+    public float bulletSelfDestroyTime = 1f;
+    public int bulletDamage = 2;
+    public int bulletHeadDamage = 4;
+
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * bulletSpeed;
+
+        Destroy(gameObject, bulletSelfDestroyTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        //col.gameObject.SendMessage("ApplyDamage", bulletDamage, SendMessageOptions.DontRequireReceiver);
+        ZombieScript zs = col.gameObject.GetComponent<ZombieScript>();
+
+        if (zs != null)
+        {
+            zs.ApplyDamage(bulletDamage);            
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/FollowCursor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour {
+
+    //public Transform balaSpawn;
+    public Vector3 balaSpawnGlobalRotation;
+
+    public Vector2 direction;
+    public float angle;
+    public Quaternion rotation;
+    public float angleFix = 0;
+    public Vector3 mousePosition;
+    public bool clampPlayer = false;
+    public bool clampGun = false;
+
+    void Start() {
+        PointToCursorManager.Instance.Subscribe(PointToCursor);
+    }
+
+    // Use this for initialization
+    void FixedUpdate ()
+    {
+        PointToCursor();
+    }
+
+    private void PointToCursor()
+    {
+        //balaSpawnGlobalRotation = balaSpawn.rotation.eulerAngles;
+        //direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - balaSpawn.position;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + angleFix;
+        if (clampPlayer)
+        {
+            angle = Mathf.Clamp(angle, 10f, 180f);
+        }
+        if (clampGun)
+        {
+            angle = Mathf.Clamp(angle, -100f, 100f);
+        }
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
